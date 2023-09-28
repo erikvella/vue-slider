a260708d81d9996181d2921e20c00078e3d683c4
erikvella <vella.erik1@gmail.com>
2023-09-28 18:19:30 +0100

    [fix] @click anteprima

--- js/script.js
+++ js/script.js
@@ -3,8 +3,41 @@ const {createApp} = Vue ;
 createApp({
 data(){
   return{
-    
+    posters:[
+       'img/01.webp',
+       'img/02.webp',
+       'img/03.webp',
+       'img/04.webp',
+       'img/05.webp',
+    ],
+    counter : 0,
   }
+},
+
+methods:{
+   nextPrev(isNext){
+    if(isNext){
+      console.log('Avanti');
+      this.counter++;
+    }else{
+      console.log('Indietro');
+      this.counter--;
+    }
+
+    // versione compatta(ternario)
+    // isNext ? this.counter++ : this.counter--;
+// controllo di validità del counter
+if(this.counter === this.posters.length){
+  this.counter = 0 ;
 }
+if(this.counter < 0){
+  this.counter = this.posters.length -1;
+}
+   }
+},
+
+mounted(){
+
+},
 }).mount('#app');
 
